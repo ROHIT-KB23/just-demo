eef1dda6fe0472bdc8d3b8f18992284b384553b0
MaitriAI-team <dev2.team@worldoffinrep.com>
2025-11-17 18:44:39 +0530

    Update UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,19 @@
-
-
-
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'MAITRI - Future of Diamonds',
-  description: 'Lab-grown sustainable diamonds with environmental stewardship',
-}
+  title: "MAITRI - Future of Diamonds",
+  description: "Lab-grown sustainable diamonds with environmental stewardship",
+};
 
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-        <head>
-        <link
-          rel="preload"
-          as="image"
-          href="/images/Hero_section.png"
-        />
+      <head>
+        <link rel="preload" as="image" href="/images/Hero_section.png" />
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
